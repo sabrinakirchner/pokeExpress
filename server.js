d82086b66b988567ee481f692087f946f85cd6b5
sabrinaK <sabrinapsouza@hotmail.com>
2023-08-23 18:26:29 -0400

    My server is set up and running

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const express = require("express")
+const express = require("express");
+const pokemon = require('./models/pokemon');//should import pokemon data
 
-const server = express()
-const PORT = process.env.PORT || 3000
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+
+//pokemon.js data.json
+server.get('/pokemon',(req,res)=> {
+    res.json(pokemon);
+}); 
+
 
 server.get("/", (req, res)=>{
     res.status(200).json({message: "Welcome to the Pokemon app!"})
-})
+});
 
 server.listen(PORT, ()=>{
     console.log(`Server is listening at: ${PORT}`)
-})
+});
